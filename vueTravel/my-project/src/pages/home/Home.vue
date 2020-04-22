5d2e4d079541d27af7b95fc0dcd2b512b8c75bab
mac <guojinfa1@163.com>
2020-04-22 20:36:50 +0800

    add

--- vueTravel/my-project/src/pages/home/Home.vue
+++ vueTravel/my-project/src/pages/home/Home.vue
@@ -3,15 +3,17 @@
       <home-header></home-header>
       <home-swiper></home-swiper>
       <home-Icons></home-Icons>
+      <home-Recommend></home-Recommend>
   </div>
 </template>
 <script>
 import HomeHeader from './components/header'
 import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
+import HomeRecommend from './components/Recommend'
 export default {
   name:'Home',
-  components:{HomeHeader,HomeSwiper,HomeIcons}
+  components:{HomeHeader,HomeSwiper,HomeIcons,HomeRecommend}
 }
 </script>
 <style>
---
+++ vueTravel/my-project/src/pages/home/components/Recommend.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="ReCommendWrap">
+       <div class ="recommend-title">热销推荐</div>
+       <div class="recommend-list border-bottom">
+         <div class="item" v-for="item of recommendList" :key="item.id">
+          <div class="imgWrap">
+            <img :src="item.src"/>
+          </div>
+          <div class="dicwrap">
+          <p class="wrapinfotitle">{{item.title}}</p>
+          <p class="wrapinfodesc">{{item.des}}</p>
+          <button class="item-button">查看详情</button>
+          </div>
+         </div>
+        
+       </div>
+       
+  </div>
+</template>
+<script>
+export default {
+  name:'HomeRecommend',
+  data () {
+    return {
+       recommendList:[{
+         id:'001',
+         src:'',
+         title:'大连圣亚海洋世界',
+         des:'宾不知道有啥好玩的'
+       },{
+         id:'002',
+         src:'',
+         title:'大连圣亚海洋世界',
+         des:'宾不知道有啥好玩的'
+       }]
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+.ReCommendWrap
+  background:#eee;
+  clear:botn;
+.recommend-title
+  line-height :0.8rem;
+  margin-top:.2rem;
+  background:#eee;
+  text-align:left;
+  text-indent:.2rem;
+  font-size:0.4rem;
+  
+.recommend-list
+.item
+  width:100%;
+  margin-bottom:.2rem;
+  background:#fff;
+  clear both
+  overflow:hidden
+  .imgWrap
+    float left
+    width:2.5rem;
+    img 
+      height 100px;
+      width:100px;
+  .dicwrap
+    float left
+    .wrapinfotitle
+      font-size:.4rem;
+    .wrapinfodesc
+      font-size:.3rem;
+      color:#333;
+    .item-button
+      font-size:.3rem;
+      padding:.3 .5rem;
+      border:1px solid #333;
+      border-radius:.1rem;  
+      background:#fff;
+</style>
